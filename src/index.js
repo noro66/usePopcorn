d347c2ff8dc94cfe8a8d23e0d1b78d47920eb390
Nouaamane <norou6@gmail.com>
2024-10-21 10:11:14 +0100

    feat: making the rating component reusable

--- src/index.js
+++ src/index.js
@@ -8,6 +8,13 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      color={"#fcc419"}
+      size={48}
+      className="test"
+      defaultRating={3}
+    />
   </React.StrictMode>
 );
